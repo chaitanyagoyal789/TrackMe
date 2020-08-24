fff0336d374692fb848606656e73ced39bcd1cb7
Chaitanya Goyal <chaitanyagoyal789@gmail.com>
2020-08-24 22:05:10 +1000

    I should have done this before now

--- mqtt/mqtt.js
+++ mqtt/mqtt.js
@@ -1,8 +1,19 @@
 const mqtt = require('mqtt');
+const dotenv = require('dotenv');
 const express = require('express');
+const mongoose = require('mongoose');
+const Device = require('../Api/models/device');
+const randomCoordinates = require('random-coordinates');
+const rand = require('random-int');
+mongoose.connect('mongodb+srv://goyalch:chaitanya123@cluster0.vbr64.mongodb.net', { useNewUrlParser: true, useUnifiedTopology: true });
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.use(express.static('public'));
 app.use((req, res, next) => {
@@ -10,16 +21,34 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-RequestedWith, Content-Type, Accept");
     next();
 });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+
 const client = mqtt.connect("mqtt://broker.hivemq.com:1883");
 client.on('connect', () => {
+    client.subscribe('/sensorData');
     console.log('mqtt connected');
 });
 
-client.subscribe('/sensorData');
+app.post('/send-command', (req, res) => {
+    const { deviceId, command } = req.body;
+    const topic = `/218570712/command/${deviceId}`;
+    client.publish(topic, command, () => {
+        res.send('published new message');
+    });
+});
+
+/**
+ *  @api {post}/send-command AllDevices An array of all devices
+ *  @apiGroup Device
+ *  @apiSuccessExample {String} Success-Response:
+ *  {
+ *   published new message
+ *  }
+ *  @apiErrorExample {string} Error-Response:
+ *  {
+ *  null
+ *  }
+ * /
+
 client.on('message', (topic, message) => {
     if (topic == '/sensorData') {
         const data = JSON.parse(message);
@@ -42,13 +71,34 @@ client.on('message', (topic, message) => {
     }
 });
 
-app.post('/send-command', (req, res) => {
-    const { deviceId, command } = req.body;
-    const topic = `/218570712/command/${deviceId}`;
-    client.publish(topic, command, () => {
+app.put('/sensor-data', (req, res) => {
+    const { deviceId } = req.body;
+    const [lat, lon] = randomCoordinates().split(", ");
+    const ts = new Date().getTime();
+    const loc = { lat, lon };
+    const temp = rand(20, 50);
+    const topic = `/sensorData`;
+    const message = JSON.stringify({ deviceId, ts, loc, temp });
+    client.publish(topic, message, () => {
         res.send('published new message');
     });
 });
+
+/**
+ * @api {put}/sensor-data81* @apiGroup Device
+ * @apiSuccessExample {string} Success-Response:
+ * [
+ *  {
+ *   "deviceId": "apple"
+ *  }
+ *  published new message
+ * ]
+ *  @apiErrorExample {string} Error-Response:
+ *  {
+ *  null
+ *  }
+ * /
+
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
